ffa902a60060be7ce0d7b90ed8f98ff00e648a84
PoorRug <>
2020-10-17 17:56:48 +0100

    Fix poor rug address

--- src/constants/tokenAddresses.ts
+++ src/constants/tokenAddresses.ts
@@ -1,2 +1,2 @@
 export const yam = '0xe5868468Cb6Dd5d6D7056bd93f084816c6eF075f'
-export const yamv2 = '0x7fD0AFab4574dd6aF43cDd1196a33dea5A4bC48a'
+export const yamv2 = '0x9d24b60d5eefe710348cc59173b166a256cbffcd'
