60ee7eee509417eb831771fd6c545d0aae48ef8c
PoorRug <stephenhastings351@gmail.com>
2020-10-19 05:56:52 +0800

    Improved FAQ

--- src/views/FAQ/FAQ.tsx
+++ src/views/FAQ/FAQ.tsx
@@ -17,20 +17,23 @@ const FAQ: React.FC = () => {
             <p>Please see below for the most up to date information regarding the future of PoorRug. As has always been our intention, the community will be in control of future decision making. The migration from Fag to Poor is the first step in this process; from there, Poor tokenholders will signal their desired path forward on community proposals.</p>
             <StyledHeading>1. How does the migration process work?</StyledHeading>
             <StyledList>
-              <StyledListItem>A new $Poor ERC-20 token will be created when you migrate. This will be the governance token of the Poor protocol.</StyledListItem>
+              <StyledListItem>A new $Poor ERC-20 token will be created when you migrate. This will be the governance token of PoorRug.</StyledListItem>
               <StyledListItem>The migration contract has been audited internally and has been deployed.</StyledListItem>
-              <StyledListItem>All Fag holders can burn Fag to mint Poor via a migration contract.</StyledListItem>
-              <StyledListItem>The number of Poor tokens received will be based upon the number of Fag tokens migrated, plus 300K which will serve as liquidity incentives for the LP Pool.</StyledListItem>
+              <StyledListItem>All $FAG holders can burn $FAG to mint $Poor via a migration contract.</StyledListItem>
+              <StyledListItem>The number of Poor tokens created will be based upon the number of $FAG tokens migrated, plus 300K which will serve as liquidity incentives for the LP Pool, this will be announced after migration is closed.</StyledListItem>
             </StyledList>
             <StyledHeading>2. Why did we create Poor?</StyledHeading>
             <StyledList>
-              <StyledListItem>Many may be familiar with the PoorFag memecoin, known as $FAG
-                It was created as a copy of $BONK with 2% staking rewards every transaction and 5% unstaking fee also going to the staking pool. The project has developed an NFT marketplace and has gained some attention on Twitter.
-                Unfortunately, the developer of this project (Telegram: @bestetherdapps aka Joe Blow) has decided to sell his stake, and claimed he did this without warning to not cause panic sell. Obviously this got some heat going on in the community, therefore we have decided to create another project that would be 1:1 token swap for $FAG token.
+              <StyledListItem>Many of you may be familiar with the PoorFag ($FAG) memecoin,
+                known as $FAG. It was created as a copy of $BONK with 2% transfer fee for every
+                transaction, and a staking pool to collect the transfer fees, to exit the pool
+                there is a 5% unstaking fee also going to the staking pool.
               </StyledListItem>
               <StyledListItem>
-                Why another project? There is already an established community around the $Fag token, it makes sense bring them over to the Poor protocol.
+                $FAG gained a decent amount of attention on Twitter. Unfortunately, the developer of the project (Telegram: @bestetherdapps aka Joe Blow) decided to sell his stake, and claimed he did this without warning in order to not cause a panic sell. Obviously this was not well received by the community and a massive price drop followed, therefore we have decided to create another project that would be 1:1 token swap for $FAG token.
               </StyledListItem>
+              <StyledListItem>
+                Why another project? There is already an established community around the $FAG token, it makes sense bring them over to PoorFag. </StyledListItem>
             </StyledList>
             <StyledHeading>3. What is PoorRug?</StyledHeading>
             <StyledList>
@@ -48,7 +51,7 @@ const FAQ: React.FC = () => {
               <StyledListItem>PoorFag:  <a href="https://etherscan.io/token/0xe5868468Cb6Dd5d6D7056bd93f084816c6eF075f">0xe5868468Cb6Dd5d6D7056bd93f084816c6eF075f</a></StyledListItem>
               <StyledListItem>Migration:  <a href="https://etherscan.io/address/0x4a6c103a1a60f7ba7b7ce832a32d1b747529f8e7">0x4a6c103a1a60f7ba7b7ce832a32d1b747529f8e7</a></StyledListItem>
               <StyledListItem>PoorRug:  <a href="https://etherscan.io/address/0x9d24b60d5eefe710348cc59173b166a256cbffcd">0x9d24b60d5eefe710348cc59173b166a256cbffcd</a></StyledListItem>
-              <StyledListItem>Uniswap:  <a href="https://info.uniswap.org/pair/0xe0a0d928c95369912ee24cbe9178cfe7f8039c2c">0xe0a0d928c95369912ee24cbe9178cfe7f8039c2c</a></StyledListItem>
+              <StyledListItem>Uniswap:  <a href="https://info.uniswap.org/pair/0xe0a0d928c95369912ee24cbe9178cfe7f8039c2c">info.uniswap.org/pair/0xe0a0d928c95369912ee24cbe9178cfe7f8039c2c</a></StyledListItem>
             </StyledList>
           </CardContent>
         </Card>
